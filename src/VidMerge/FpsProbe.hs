82b6b838cf2781e175ed1cc13bcd82e0d2b88040
Lucas G <g@11xx.org>
2023-07-02 00:28:29 -0300

    Create file FpsProbe
    - I hadn't commited it before, oops

---
+++ src/VidMerge/FpsProbe.hs
@@ -0,0 +1,34 @@
+module VidMerge.FpsProbe where
+
+import System.Process
+    ( createProcess,
+      proc,
+      CreateProcess(std_out),
+      StdStream(CreatePipe) )
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C
+import System.IO ( hGetContents )
+import Data.Char (isSpace)
+
+fpsProbe :: String -> IO [Char]
+fpsProbe f = do
+  (_, Just out, _, _) <- createProcess $ (fpsProbeCmd f) { std_out = CreatePipe }
+  hout <- hGetContents out
+
+  pure $ rstrip hout
+
+fpsProbeCmd :: String -> CreateProcess
+fpsProbeCmd f =
+  proc "ffprobe"
+       [ "-v", "0"
+       , "-of", "csv=p=0"
+       , "-select_streams"
+       , "v:0"
+       , "-show_entries"
+       , "stream=r_frame_rate"
+       , f
+       ]
+
+-- remove \n at the end
+rstrip :: [Char] -> [Char]
+rstrip = reverse . dropWhile isSpace . reverse
