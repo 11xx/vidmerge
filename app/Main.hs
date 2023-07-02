0e7c1544e6802a25aaa86efbbc21b35fbb34a316
Lucas G <g@11xx.org>
2023-07-02 00:03:15 -0300

    Add fps probe
    - testing functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,25 @@
 module Main where
 
 import VidMerge.FrameMd5 ( writeFrameIndexToFile )
+import VidMerge.FpsProbe ( fpsProbe )
 
-import Options
+import Options ( optsParserInfo, execParser, Opts(Opts) )
 
 main :: IO ()
 main = do
   opts <- execParser optsParserInfo
   frameIndexFromArgs opts
+  fpsProbePrint opts
 
-frameIndexFromArgs opts@(Opts file1 file2 _) = do
-  writeFrameIndexToFile file1
-  writeFrameIndexToFile file2
+frameIndexFromArgs :: Opts -> IO ()
+frameIndexFromArgs opts@(Opts f1 f2 _) = do
+  writeFrameIndexToFile f1
+  writeFrameIndexToFile f2
+
+fpsProbePrint :: Opts -> IO ()
+fpsProbePrint opts@(Opts f1 f2 _) = do
+  pf1 <- fpsProbe f1
+  putStrLn $ unwords [f1, "has fps", pf1]
+
+  pf2 <- fpsProbe f2
+  putStrLn $ unwords [f2, "has fps", pf2]
